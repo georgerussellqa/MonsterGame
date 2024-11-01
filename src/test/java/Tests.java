c28b396ccf08c5694a36be75ab51ea4c5e7816a3
seb <sebsmithqa@gmail.com>
2024-11-01 11:38:02 +0000

    tests

---
+++ src/test/java/Tests.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Tests {
+    @Test
+    void creating_0_by_0_board_fails(){
+
+    }
+
+    @Test
+    void creating_neg_1_by_neg_1_board_fails(){
+
+    }
+
+    @Test
+    void creating_1_by_1_board_fails(){
+
+    }
+
+    @Test
+    void creating_2_by_2_board_creates_board_object(){
+
+    }
+
+    @Test
+    void creating_more_than_2_by_more_than_2_board_creates_board_object(){
+
+    }
+}
